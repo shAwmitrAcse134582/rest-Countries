0a716e981e5b991f1dfff2be15ca5748550cec73
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2023-03-16 02:50:33 +0600

    destructuring

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const loadCountries = () => {
     fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
@@ -14,11 +15,38 @@ const displayCountries = countries => {
 
 }
 
-const getCountryHtml = country => {
+
+//*Orginal way
+// const getCountryHtml = country => {
+//     return `
+//     <div class="country">
+//            <h2>${country.name.common}</h2>
+//            <img src="${country.flags.png}">
+//     </div>
+//     `
+// }
+
+//*option-2 using destructuring
+
+// const getCountryHtml = country => {
+//   
+//     const { name, flags } = country;
+//     return `
+//     <div class="country">
+//            <h2>${name.common}</h2>
+//            <img src="${flags.png}">
+//     </div>
+//     `
+// }
+//*option-3 direct accessing value by parameter
+const getCountryHtml = ({ name, flags, area, region }) => {
+    // const {name,flags}=country;
     return `
     <div class="country">
-           <h2>${country.name.common}</h2>
-           <img src="${country.flags.png}">
+           <h2>${name.common}</h2>
+           <p>Area:${area}</p>
+           <p>Region:${region}</p>
+           <img src="${flags.png}">
     </div>
     `
 }
